2c5fa526b374dde8915fd902a47d206f3a3246a1
Binux <17175297.hk@gmail.com>
2014-02-22 03:19:37 +0800

    update

--- libs/dataurl.py
+++ libs/dataurl.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 # vim: set et sw=4 ts=4 sts=4 ff=unix fenc=utf8:
-# Author: Binux<17175297.hk@gmail.com>
+# Author: Binux<i@binux.me>
 #         http://binux.me
 # Created on 2012-11-16 10:33:20
 
