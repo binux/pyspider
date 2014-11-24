1e69d8800c8763cf2d3ddd54039d8c6d78c7cbee
binux <roy@binux.me>
2014-11-25 00:02:33 +0800

    update setup.py add programming language

--- setup.py
+++ setup.py
@@ -31,6 +31,8 @@ setup(
 
         classifiers=[
             'Development Status :: 3 - Alpha',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
 
             'License :: OSI Approved :: Apache Software License',
 
