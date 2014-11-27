27c09d94ad688b0b763417218da851892da197b0
binux <roy@binux.me>
2014-11-27 17:40:55 +0800

    add test for phantomjs

--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -5,9 +5,11 @@
 #         http://binux.me
 # Created on 2014-02-15 22:10:35
 
+import os
 import time
 import umsgpack
 import xmlrpclib
+import subprocess
 import unittest2 as unittest
 from multiprocessing import Queue
 
@@ -26,6 +28,9 @@ class TestFetcher(unittest.TestCase):
                 'Cookie': 'a=b',
                 'a': 'b'
             },
+            'cookies': {
+                'c': 'd',
+            },
             'timeout': 60,
             'save': 'abc',
         },
@@ -40,12 +45,19 @@ class TestFetcher(unittest.TestCase):
         self.inqueue = Queue(10)
         self.outqueue = Queue(10)
         self.fetcher = Fetcher(self.inqueue, self.outqueue)
+        self.fetcher.phantomjs_proxy = 'localhost:25555'
         self.rpc = xmlrpclib.ServerProxy('http://localhost:%d' % 24444)
         self.xmlrpc_thread = utils.run_in_thread(self.fetcher.xmlrpc_run, port=24444)
         self.thread = utils.run_in_thread(self.fetcher.run)
+        self.phantomjs = subprocess.Popen(['phantomjs',
+            os.path.join(os.path.dirname(__file__),
+                '../pyspider/fetcher/phantomjs_fetcher.js'),
+            '25555'])
 
     @classmethod
     def tearDownClass(self):
+        self.phantomjs.kill()
+        self.phantomjs.wait()
         self.rpc._quit()
         self.thread.join()
         time.sleep(1)
@@ -58,9 +70,9 @@ class TestFetcher(unittest.TestCase):
         self.assertIn('content', result)
 
         content = result['content']
-        self.assertIn('..A:', content)
-        self.assertIn('..Cookie:', content)
-        self.assertIn('a=b', content)
+        self.assertIn('<b>A:', content)
+        self.assertIn('<b>Cookie:</b>', content)
+        self.assertIn('c=d</td>', content)
 
     def test_10_http_post(self):
         request = dict(self.sample_task_http)
@@ -118,3 +130,47 @@ class TestFetcher(unittest.TestCase):
         self.assertIn(' d0 ', result['content'])
         self.assertIn(' ce ', result['content'])
         self.assertIn(' c4 ', result['content'])
+
+    def test_60_timeout(self):
+        request = dict(self.sample_task_http)
+        request['url'] = 'http://httpbin.org/delay/10'
+        request['fetch']['timeout'] = 3
+        start_time = time.time()
+        self.inqueue.put(request)
+        task, result = self.outqueue.get()
+        end_time = time.time()
+        self.assertGreater(end_time - start_time, 2)
+        self.assertLess(end_time - start_time, 4)
+
+    def test_70_phantomjs_url(self):
+        request = dict(self.sample_task_http)
+        request['fetch']['fetch_type'] = 'js'
+        result = self.fetcher.sync_fetch(request)
+        self.assertEqual(result['status_code'], 200)
+        self.assertEqual(result['orig_url'], self.sample_task_http['url'])
+        self.assertEqual(result['save'], self.sample_task_http['fetch']['save'])
+        self.assertIn('content', result)
+
+        content = result['content']
+        self.assertIn('<b>a:</b>', content)
+        self.assertIn('<b>Cookie:</b>', content)
+        self.assertIn('c=d</td>', content)
+
+    def test_80_phantomjs_timeout(self):
+        request = dict(self.sample_task_http)
+        request['url'] = 'http://httpbin.org/delay/10'
+        request['fetch']['fetch_type'] = 'js'
+        request['fetch']['timeout'] = 3
+        start_time = time.time()
+        result = self.fetcher.sync_fetch(request)
+        end_time = time.time()
+        self.assertGreater(end_time - start_time, 2)
+        self.assertLess(end_time - start_time, 4)
+
+    def test_90_phantomjs_js_script(self):
+        request = dict(self.sample_task_http)
+        request['fetch']['fetch_type'] = 'js'
+        request['fetch']['js_script'] = 'function() { document.write("binux") }'
+        result = self.fetcher.sync_fetch(request)
+        self.assertEqual(result['status_code'], 200)
+        self.assertIn('binux', result['content'])
