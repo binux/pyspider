a81be8b620f715f03922b5465f736a6323e27d92
binux <roy@binux.me>
2015-02-05 17:40:01 +0800

    fix #119

--- pyspider/webui/login.py
+++ pyspider/webui/login.py
@@ -38,10 +38,11 @@ def load_user_from_request(request):
     if api_key:
         api_key = api_key.replace('Basic ', '', 1)
         try:
-            api_key = base64.b64decode(api_key)
-        except TypeError:
-            pass
-        return User(*api_key.split(":", 1))
+            api_key = base64.b64decode(api_key).decode('utf8')
+            return User(*api_key.split(":", 1))
+        except Exception as e:
+            app.logger.error('wrong api key: %r, %r', api_key, e)
+            return None
     return None
 app.login_response = Response(
     "need auth.", 401, {'WWW-Authenticate': 'Basic realm="Login Required"'}
