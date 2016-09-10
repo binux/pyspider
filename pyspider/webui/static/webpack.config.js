be2ed33cdcea3dca8063115bb7c8e6109bcd8f73
binux <roy@binux.me>
2016-09-10 18:08:55 +0100

    change tab to space

--- pyspider/webui/static/webpack.config.js
+++ pyspider/webui/static/webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
     debug: "./src/debug",
     css_selector_helper: "./src/css_selector_helper",
     result: "./src/result.less",
-		task: "./src/task.less",
-		tasks: "./src/tasks.less",
+    task: "./src/task.less",
+    tasks: "./src/tasks.less",
   },
   output: {
     //path: "./dist",
@@ -19,8 +19,7 @@ module.exports = {
       { test: /\.less$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader") }
     ]
   },
-  //devtool: "#inline-source-map",
-	devtool: 'source-map',
+  devtool: 'source-map',
   plugins: [
     new ExtractTextPlugin("[name].css")
   ]
