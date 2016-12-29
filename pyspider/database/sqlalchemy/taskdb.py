c6a89d56cd6bef2a395ac207081335c2173138ac
binux <roy@binux.me>
2016-12-29 11:41:41 +0000

    fix UnicodeEncodeError when use sqlalchemy with mysql, fix #594

--- pyspider/database/sqlalchemy/taskdb.py
+++ pyspider/database/sqlalchemy/taskdb.py
@@ -43,14 +43,14 @@ class TaskDB(SplitTableMixin, BaseTaskDB):
             database = self.url.database
             self.url.database = None
             try:
-                engine = create_engine(self.url, pool_recycle=3600)
+                engine = create_engine(self.url, convert_unicode=True, pool_recycle=3600)
                 conn = engine.connect()
                 conn.execute("commit")
                 conn.execute("CREATE DATABASE %s" % database)
             except sqlalchemy.exc.SQLAlchemyError:
                 pass
             self.url.database = database
-        self.engine = create_engine(url, pool_recycle=3600)
+        self.engine = create_engine(url, convert_unicode=True, pool_recycle=3600)
 
         self._list_project()
 
