01f125ce926e116d959a3726289f21713fa3530b
M-swords <35534833+M-swords@users.noreply.github.com>
2018-03-07 06:24:32 +0100

    Refactor utils connect db (#769)
    * part 1
    
    * [Doc] Update coverage links for the fork
    
    * Refactor. Changes to utils and _connect_db
    
    * Refactored in utils
    
    Refactored one of db connections. more to follow.
    
    * Refactor. Split up some more connects
    
    * Rm. Removed unused textfile
    
    * Doc. rewrote README.md to original
    
    * Refactor. Wrong variable name fix.
    
    * Refactor, applied suggested changes in the review in the database connector.
    
    * Refactor, changed name of return variable gotten from fix_full_format in format_date

--- pyspider/database/__init__.py
+++ pyspider/database/__init__.py
@@ -61,83 +61,17 @@ def _connect_database(url):  # NOQA
                           'type should be one of ["taskdb", "projectdb", "resultdb"]', dbtype)
 
     if engine == 'mysql':
-        parames = {}
-        if parsed.username:
-            parames['user'] = parsed.username
-        if parsed.password:
-            parames['passwd'] = parsed.password
-        if parsed.hostname:
-            parames['host'] = parsed.hostname
-        if parsed.port:
-            parames['port'] = parsed.port
-        if parsed.path.strip('/'):
-            parames['database'] = parsed.path.strip('/')
+        return _connect_mysql(parsed,dbtype)
 
-        if dbtype == 'taskdb':
-            from .mysql.taskdb import TaskDB
-            return TaskDB(**parames)
-        elif dbtype == 'projectdb':
-            from .mysql.projectdb import ProjectDB
-            return ProjectDB(**parames)
-        elif dbtype == 'resultdb':
-            from .mysql.resultdb import ResultDB
-            return ResultDB(**parames)
-        else:
-            raise LookupError
     elif engine == 'sqlite':
-        if parsed.path.startswith('//'):
-            path = '/' + parsed.path.strip('/')
-        elif parsed.path.startswith('/'):
-            path = './' + parsed.path.strip('/')
-        elif not parsed.path:
-            path = ':memory:'
-        else:
-            raise Exception('error path: %s' % parsed.path)
-
-        if dbtype == 'taskdb':
-            from .sqlite.taskdb import TaskDB
-            return TaskDB(path)
-        elif dbtype == 'projectdb':
-            from .sqlite.projectdb import ProjectDB
-            return ProjectDB(path)
-        elif dbtype == 'resultdb':
-            from .sqlite.resultdb import ResultDB
-            return ResultDB(path)
-        else:
-            raise LookupError
+        return _connect_sqlite(parsed,dbtype)
     elif engine == 'mongodb':
-        url = url.replace(parsed.scheme, 'mongodb')
-        parames = {}
-        if parsed.path.strip('/'):
-            parames['database'] = parsed.path.strip('/')
+        return _connect_mongodb(parsed,dbtype,url)
 
-        if dbtype == 'taskdb':
-            from .mongodb.taskdb import TaskDB
-            return TaskDB(url, **parames)
-        elif dbtype == 'projectdb':
-            from .mongodb.projectdb import ProjectDB
-            return ProjectDB(url, **parames)
-        elif dbtype == 'resultdb':
-            from .mongodb.resultdb import ResultDB
-            return ResultDB(url, **parames)
-        else:
-            raise LookupError
     elif engine == 'sqlalchemy':
-        if not other_scheme:
-            raise Exception('wrong scheme format: %s' % parsed.scheme)
-        url = url.replace(parsed.scheme, other_scheme)
+        return _connect_sqlalchemy(parsed, dbtype, url, other_scheme)
 
-        if dbtype == 'taskdb':
-            from .sqlalchemy.taskdb import TaskDB
-            return TaskDB(url)
-        elif dbtype == 'projectdb':
-            from .sqlalchemy.projectdb import ProjectDB
-            return ProjectDB(url)
-        elif dbtype == 'resultdb':
-            from .sqlalchemy.resultdb import ResultDB
-            return ResultDB(url)
-        else:
-            raise LookupError
+
     elif engine == 'redis':
         if dbtype == 'taskdb':
             from .redis.taskdb import TaskDB
@@ -153,24 +87,114 @@ def _connect_database(url):  # NOQA
         else:
             raise LookupError('not supported dbtype: %s', dbtype)
     elif engine == 'elasticsearch' or engine == 'es':
-        # in python 2.6 url like "http://host/?query", query will not been splitted
-        if parsed.path.startswith('/?'):
-            index = parse_qs(parsed.path[2:])
-        else:
-            index = parse_qs(parsed.query)
-        if 'index' in index and index['index']:
-            index = index['index'][0]
-        else:
-            index = 'pyspider'
+        return _connect_elasticsearch(parsed, dbtype)
 
-        if dbtype == 'projectdb':
-            from .elasticsearch.projectdb import ProjectDB
-            return ProjectDB([parsed.netloc], index=index)
-        elif dbtype == 'resultdb':
-            from .elasticsearch.resultdb import ResultDB
-            return ResultDB([parsed.netloc], index=index)
-        elif dbtype == 'taskdb':
-            from .elasticsearch.taskdb import TaskDB
-            return TaskDB([parsed.netloc], index=index)
     else:
         raise Exception('unknown engine: %s' % engine)
+
+
+def _connect_mysql(parsed,dbtype):
+    parames = {}
+    if parsed.username:
+        parames['user'] = parsed.username
+    if parsed.password:
+        parames['passwd'] = parsed.password
+    if parsed.hostname:
+        parames['host'] = parsed.hostname
+    if parsed.port:
+        parames['port'] = parsed.port
+    if parsed.path.strip('/'):
+        parames['database'] = parsed.path.strip('/')
+
+    if dbtype == 'taskdb':
+        from .mysql.taskdb import TaskDB
+        return TaskDB(**parames)
+    elif dbtype == 'projectdb':
+        from .mysql.projectdb import ProjectDB
+        return ProjectDB(**parames)
+    elif dbtype == 'resultdb':
+        from .mysql.resultdb import ResultDB
+        return ResultDB(**parames)
+    else:
+        raise LookupError
+
+
+def _connect_sqlite(parsed,dbtype):
+    if parsed.path.startswith('//'):
+        path = '/' + parsed.path.strip('/')
+    elif parsed.path.startswith('/'):
+        path = './' + parsed.path.strip('/')
+    elif not parsed.path:
+        path = ':memory:'
+    else:
+        raise Exception('error path: %s' % parsed.path)
+
+    if dbtype == 'taskdb':
+        from .sqlite.taskdb import TaskDB
+        return TaskDB(path)
+    elif dbtype == 'projectdb':
+        from .sqlite.projectdb import ProjectDB
+        return ProjectDB(path)
+    elif dbtype == 'resultdb':
+        from .sqlite.resultdb import ResultDB
+        return ResultDB(path)
+    else:
+        raise LookupError
+
+
+def _connect_mongodb(parsed,dbtype,url):
+    url = url.replace(parsed.scheme, 'mongodb')
+    parames = {}
+    if parsed.path.strip('/'):
+        parames['database'] = parsed.path.strip('/')
+
+    if dbtype == 'taskdb':
+        from .mongodb.taskdb import TaskDB
+        return TaskDB(url, **parames)
+    elif dbtype == 'projectdb':
+        from .mongodb.projectdb import ProjectDB
+        return ProjectDB(url, **parames)
+    elif dbtype == 'resultdb':
+        from .mongodb.resultdb import ResultDB
+        return ResultDB(url, **parames)
+    else:
+        raise LookupError
+
+
+def _connect_sqlalchemy(parsed, dbtype,url, other_scheme):
+    if not other_scheme:
+        raise Exception('wrong scheme format: %s' % parsed.scheme)
+    url = url.replace(parsed.scheme, other_scheme)
+    if dbtype == 'taskdb':
+        from .sqlalchemy.taskdb import TaskDB
+        return TaskDB(url)
+    elif dbtype == 'projectdb':
+        from .sqlalchemy.projectdb import ProjectDB
+        return ProjectDB(url)
+    elif dbtype == 'resultdb':
+        from .sqlalchemy.resultdb import ResultDB
+        return ResultDB(url)
+    else:
+        raise LookupError
+
+
+def _connect_elasticsearch(parsed, dbtype):
+    # in python 2.6 url like "http://host/?query", query will not been splitted
+    if parsed.path.startswith('/?'):
+        index = parse_qs(parsed.path[2:])
+    else:
+        index = parse_qs(parsed.query)
+    if 'index' in index and index['index']:
+        index = index['index'][0]
+    else:
+        index = 'pyspider'
+
+    if dbtype == 'projectdb':
+        from .elasticsearch.projectdb import ProjectDB
+        return ProjectDB([parsed.netloc], index=index)
+    elif dbtype == 'resultdb':
+        from .elasticsearch.resultdb import ResultDB
+        return ResultDB([parsed.netloc], index=index)
+    elif dbtype == 'taskdb':
+        from .elasticsearch.taskdb import TaskDB
+        return TaskDB([parsed.netloc], index=index)
