7c205fff394690d0105575c1c6d2945b7ec5b7da
土豆花 <vivienne@tudouhuadeiMac.local>
2018-06-07 00:22:47 +0800

    增加了点击和滚动的事件，调试还没成功

--- pyspider/fetcher/chromeheadless.js
+++ pyspider/fetcher/chromeheadless.js
@@ -28,83 +28,109 @@ server = http.createServer(function(request,response){
 
 	request.on('end',function(data){
 		(async () => {
-		    console.log("fetch的内容："+ JSON.stringify(fetch))
-			const start_time = await Date.now()
-			let end_time = await null,
-				first_response = await null,
-				finished = await false,
-				page_loaded = await false;
+			try{
+				await console.log("fetch的内容："+ JSON.stringify(fetch))
+				const start_time = await Date.now()
+				let end_time = await null,
+					first_response = await null,
+					finished = await false,
+					page_loaded = await false;
 
-			fetch = await JSON.parse(fetch)
-			await console.log("服务器接收到的数据：　"+fetch.url);
-			// 是否启用代理
-			if (fetch.proxy && fetch.proxy.includes("://")) {
-		        fetch.proxy = '--proxy-server=' + fetch.proxy.replace(/http:\/\//,"").replace(/https:\/\//,"")
-		    }else if (fetch.proxy){
-		    	fetch.proxy = '--proxy-server=' + fetch.proxy
-		    }
-		    if (fetch.proxy) {
-		    	browser = await puppeteer.launch({
-					headless: false,
-					args: [fetch.proxy]
-				});
-		    }else{
-		    	browser = await puppeteer.connect({browserWSEndpoint}) 
-		    }
-			
-			const page = await browser.newPage();
-			// 设置浏览器视窗的大小
-			page.setViewport({
-				width:fetch.js_viewport_width || 1024,
-				height:fetch.js_viewport_width || 768*3
-			}) 
-			// 监听网页的console输出的内容
-			page.on('console', msg => {
-			  	if (typeof msg === 'object') {
-			  		console.log(msg.text())
-			  	}else{
-			  		console.log(msg)
-			  	}
-			})
+				fetch = await JSON.parse(fetch)
+				await console.log("服务器接收到的数据：　"+fetch.url);
+				// 是否启用代理
+				if (fetch.proxy && fetch.proxy.includes("://")) {
+					fetch.proxy = '--proxy-server=' + fetch.proxy.replace(/http:\/\//,"").replace(/https:\/\//,"")
+				}else if (fetch.proxy){
+					fetch.proxy = '--proxy-server=' + fetch.proxy
+				}
+				if (fetch.proxy) {
+					browser = await puppeteer.launch({
+						headless: false,
+						args: [fetch.proxy]
+					});
+				}else{
+					browser = await puppeteer.connect({browserWSEndpoint}) 
+				}
+				
+				const page = await browser.newPage();
+				// 设置浏览器视窗的大小
 
-			// 页面加载完毕时输出
-			page.once('load',() => console.log(" page load finished !!! "))
-			// 选择设备
-			if (fetch.devices) {
-				await page.emulate(devices[fetch.devices]);
-			}
-			// 翻页
-			if (fetch.next) {
-				while(true) {
-					const next = await page.$(fetch.next);
-					if (next) {
-						await next.click();
-						await page.waitFor(fetch.pageInterval)
+				// 选择设备
+				if (fetch.devices) {
+					await page.emulate(devices[fetch.devices]);
+				}else{
+					await page.setViewport({
+						width:fetch.js_viewport_width || 1024,
+						height:fetch.js_viewport_width || 768*3
+					})
+				}
+				
+				// 进入被抓取的页面
+				const response = await page.goto(fetch.url)
+				await page.waitFor(1000)
+
+				// 页面加载完毕时输出
+				await page.once('load',() => console.log(" page load finished !!! "))
+
+				// 监听网页的console输出的内容
+				await page.on('console', msg => {
+					if (typeof msg === 'object') {
+						console.log(msg.text())
 					}else{
-						break
+						console.log(msg)
+					}
+				})
+
+				// 翻页
+				if (fetch.next) {
+					while(true) {
+						const next = await page.$(fetch.next);
+						if (next) {
+							await next.click();
+							await page.waitFor(fetch.pageInterval)
+						}else{
+							break
+						}
+					}
+				}
+				await console.log('我来了！！！！！')
+				// 滚动
+				let scrollStep = await fetch.scrollStep; //每次滚动的步长
+				let scrollEnable = await fetch.scrollEnable
+				while (scrollEnable) {
+					scrollEnable = await page.evaluate(() => {
+						let scrollTop = document.scrollingElement.scrollTop;
+						document.scrollingElement.scrollTop = scrollTop + fetch.scrollStep;
+						if (fetch.scrollPosition) {
+							return fetch.scrollPosition > scrollTop + 768 ? true : false
+						}else{
+							return document.body.clientHeight > scrollTop + 768 ? true : false
+						}
+					}, fetch.scrollStep);
+					await page.waitFor(fetch.scrollTime) // 每次滚动的间隔时间
+				}
+				
+				// 点击
+				if(fetch.click_list.length !== 0){
+					const click_list = await fetch.click_list
+					for (let key in click_list) {
+						let click_ele = await page.$(click_list[key])
+						await click_ele.click()
+						page.waitFor(300)
 					}
 				}
 			}
-			// 滚动
-			let scrollStep = fetch.scrollStep; //每次滚动的步长
-			while (false) {
-				scrollEnable = await page.evaluate(() => {
-				 	let scrollTop = document.scrollingElement.scrollTop;
-				 	document.scrollingElement.scrollTop = scrollTop + fetch.scrollStep;
-				 	if (fetch.scrollPosition) {
-				 		return fetch.scrollPosition > scrollTop + 1080 ? true : false
-				 	}else{
-				 		return document.body.clientHeight > scrollTop + 1080 ? true : false
-				 	}
-			 	}, fetch.scrollStep);
-			 	await page.waitFor(fetch.scrollTime) // 每次滚动的间隔时间
+			catch(e){
+				console.log(e)
+				console.log("有错！！！！")
 			}
+		    
+
 			// 用于存储结果
-			let result = {}
+			let result = await {}
 
 			try{
-				const response = await page.goto(fetch.url)
-				await page.waitFor(1000)
 				const content = await page.content()
 				result = await {
 						orig_url: fetch.url,
@@ -131,7 +157,8 @@ server = http.createServer(function(request,response){
 			        time: (Date.now() - start_time) / 1000,
 			        js_script_result: null,
 			        save: fetch.save
-		  		}
+				}
+				page.close()
 			}
 			
 			const body = await JSON.stringify(result, null, 2)
--- pyspider/libs/base_handler.py
+++ pyspider/libs/base_handler.py
@@ -221,7 +221,7 @@ class BaseHandler(object):
     fetch_fields = ('method', 'headers', 'user_agent', 'data', 'connect_timeout', 'timeout', 'allow_redirects', 'cookies',
                     'proxy', 'etag', 'last_modifed', 'last_modified', 'save', 'js_run_at', 'js_script',
                     'js_viewport_width', 'js_viewport_height', 'load_images', 'fetch_type', 'use_gzip', 'validate_cert',
-                    'max_redirects', 'robots_txt')
+                    'max_redirects', 'robots_txt','scrollTime','scrollPosition','scrollEnable','scrollStep','click_list')
     process_fields = ('callback', 'process_time_limit')
 
     @staticmethod
