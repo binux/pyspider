c96ed3650df0a81601960adf8cf450f27321b3f3
twy <676534074@qq.com>
2018-12-17 14:20:20 +0800

    重构，修复若干BUG

--- pyspider/fetcher/chromium_fetcher.js
+++ pyspider/fetcher/chromium_fetcher.js
@@ -11,12 +11,10 @@ app.use(bodyParser());
 // to get the port which server run
 const port = process.argv[2];
 
-let result = "",
-	_fetch = "",
+const wait_before_end = 1000 ;
+let _fetch = "",
+    result = "",
 	browser = "",
-	// use for judge proxy switch ? 
-	pre_proxy = "",
-	finish = false,
 	body = "";
 
 // koa server
@@ -48,208 +46,171 @@ app.use(async (ctx,next) => {
 });
 
 // get method with puppeteer
-const get = async (_fetch) => {
-	const start_time = Date.now();
-	let response = "",
-		script_result = "",
-		content = "",
-		page = "";
-	try{
-		// use proxy ?
-		// Frequent opening and closing of the browser affects performance, so only the proxt changes will restart the browser.
-		if (_fetch.proxy) {
-			const now_proxy = _fetch.proxy;
+const get = async _fetch => {
+    return new Promise(async (resolve,rejects)  => {
+        const start_time = Date.now();
+        let response = "",
+            script_result = "",
+            content = "",
+            loaded = "",
+            end_time = "",
+            page_timeout = "",
+            finished = false,
+            page = "";
 
-			if (!_fetch.proxy.includes("://")){
-				_fetch.proxy = `--proxy-server=http://${_fetch.proxy}`;
-			}else{
-                _fetch.proxy = `--proxy-server=${_fetch.proxy}`;
+        // use proxy ?
+        if (!browser) {
+            if (_fetch.proxy) {
+
+                if (!_fetch.proxy.includes("://")){
+                    _fetch.proxy = `--proxy-server=http://${_fetch.proxy}`;
+                }else{
+                    _fetch.proxy = `--proxy-server=${_fetch.proxy}`;
+                }
+
+                browser = await puppeteer.launch({
+                    headless: _fetch.headless !== false,
+                    args: [_fetch.proxy,'--no-sandbox', '--disable-setuid-sandbox']
+                });
+
+            } else {
+                browser = await puppeteer.launch({
+                    headless: _fetch.headless !== false,
+                    args: ['--no-sandbox', '--disable-setuid-sandbox']
+                });
             }
+        }
 
-			if (browser !== "" && pre_proxy !== now_proxy) {
-				console.log(`Swith proxy to: ${_fetch.proxy}`);
-				await browser.close();
-				browser = await puppeteer.launch({
-					headless: _fetch.headless !== false,
-					timeout: _fetch.timeout ? _fetch.timeout * 1000 : 30 * 1000,
-					args: [_fetch.proxy, '--no-sandbox', '--disable-setuid-sandbox']
-				});
-			}else if (browser === ""){
-				browser = await puppeteer.launch({
-					headless: _fetch.headless !== false,
-					timeout: _fetch.timeout ? _fetch.timeout * 1000 : 30 * 1000,
-					args: [_fetch.proxy,'--no-sandbox', '--disable-setuid-sandbox']
-				});
-			}
-			
-		} else {
-			if (browser === ""){
-				browser = await puppeteer.launch({
-					headless: _fetch.headless !== false,
-					timeout:_fetch.timeout ? _fetch.timeout * 1000 : 30*1000,
-					args: ['--no-sandbox', '--disable-setuid-sandbox']
-				});
-			} else if (pre_proxy !== ""){
-				pre_proxy = "";
-				await browser.close();
-				browser = await puppeteer.launch({
-				    	headless: _fetch.headless !== false,
-					timeout:_fetch.timeout ? _fetch.timeout * 1000 : 30*1000,
-					args: ['--no-sandbox', '--disable-setuid-sandbox']
-				});
-			} 
-		}
-		// pre_proxy is last request to use there compare it to now_proxy to judge the proxy is switch ?
-		pre_proxy = _fetch.proxy;
+        // create and set page
+        page = await browser.newPage();
+        page_timeout = _fetch.timeout ? _fetch.timeout * 1000 : 20*1000;
+        page.setDefaultNavigationTimeout(page_timeout);
 
-		// create and set page
-		page = await browser.newPage();
-		await page.setRequestInterception(true);
+        await page.setRequestInterception(true);
 
-		// set user-agent
-		if (_fetch.headers && _fetch.headers['User-Agent']) {
-			await page.setUserAgent(_fetch.headers['User-Agent']);
-		}
+        // set user-agent
+        if (_fetch.headers && _fetch.headers['User-Agent']) {
+            await page.setUserAgent(_fetch.headers['User-Agent']);
+        }
 
-		// choice browse device or set page size
-		if (_fetch.device) {
-			await page.emulate(device[_fetch.device]);
-		}else{
-			await page.setViewport({
-				width:_fetch.js_viewport_width || 1024,
-				height:_fetch.js_viewport_width || 768*3
-			})
-		}
+        // choice browse device or set page size
+        if (_fetch.device) {
+            await page.emulate(device[_fetch.device]);
+        } else {
+            await page.setViewport({
+                width:_fetch.js_viewport_width || 1024,
+                height:_fetch.js_viewport_height || 768*3
+            })
+        }
 
-		// when base page load finish
-		// page.once('domcontentloaded',() => {
-		// 	console.log("base page load finished !!! ");
-		// });
+        // when base page load finish
+        page.once('domcontentloaded',async () => {
+            loaded = true;
+            console.log("base page load finished !!!");
+            // run js_script
+            if(_fetch.js_script && _fetch.js_run_at !== "document-start") {
+                script_result = await page.evaluate(_fetch.js_script);
+            }
+        });
 
-		const logRequest =  (interceptedRequest,request) => {
-			console.log('A request was made:', interceptedRequest.url());
-		};
-		page.on('request', logRequest);
+        // load images ?
+        page.on('request', request => {
+           if(request.resourceType() === 'image' && !_fetch.load_images){
+                request.abort();
+           } else {
+                console.log(`Starting request: [${request.method()}] ${request.url()} `);
+                request.continue();
+           }
+        });
 
-		// load images ?
-		if(!_fetch.load_images){
-			page.on('request',request => {
-				if (request.resourceType() === 'image')
-					request.abort();
-				else
-					request.continue();
-			})
-		}
+        // set request headers
+        page.setExtraHTTPHeaders(_fetch.headers);
 
-		// set request headers
-		page.setExtraHTTPHeaders(_fetch.headers);
+        // set cookies
+        if(_fetch.cookies) {
+            const cookies = [];
+            for(let each of _fetch.cookies){
+                cookies.push({name:each,value:_fetch.cookies[each],url:_fetch.url})
+            }
+            await page.setCookie(cookies);
+        }
 
-		// set cookies
-		if(_fetch.cookies){
-			const cookies = [];
-			for(let each in _fetch.cookies){
-				cookies.push({name:each,value:_fetch.cookies[each],url:_fetch.url})
-			}
-			await page.setCookie(cookies);
-		}
+        // print the page console messages (filter type=image if load_images=False or undefined)
+        page.on('console', msg => {
+            if (typeof msg === 'object' && msg.text() !== "Failed to load resource: net::ERR_FAILED") {
+                console.log('console:' + msg.text())
+            }
+        });
 
-		// print the page console messages
-		page.on('console', msg => {
-			if (typeof msg === 'object') {
-				console.log('console:' + msg.text())
-			}else{
-				console.log('console:' + msg)
-			}
-		});
+        // request failed
+        // page.on('requestfailed', request => {
+        // 	console.log(`failure：${request.url()} because：${request.failure().errorText}`);
+        // });
 
-		// request failed
-		// page.on('requestfailed', request => {
-		// 	console.log(`failure：${request.url()} because：${request.failure().errorText}`);
-		// });
+        page.on('requestfinished', request => {
+            console.log(`Request finished: [${request.method()}] ${request.url()}`);
+            if (loaded) {
+                end_time = Date.now() + wait_before_end;
+                make_result().then(result => resolve(result));
+            }
+        });
 
-		// to make sure request finish
-		let counter_1 = 0,
-			counter_2;
-		const counter = () => {
-			counter_1 += 1;
-		};
-		page.on('request',counter);
-
-		// every 200ms check request is completed ?
-		const make_result = () =>{
-			return new Promise((resolve,reject) => {
-				setTimeout(function(){
-					if(counter_1 === counter_2){
-						// console.log(counter_1 + " --- " + counter_2);
-						// console.log("Finish!!!");
-						resolve (true);
-					}else {
-						// console.log(counter_1 + " --- " + counter_2);
-						// console.log("Not finish");
-                        counter_2 = counter_1;
-                        resolve(make_result());
+        const make_result = () => {
+            return new Promise((resolve, reject) => {
+                setTimeout(async() => {
+                    if (finished) {
+                        return "";
                     }
-				},500)
-			})
-		};
+                    if (Date.now() - start_time < page_timeout) {
+                        if (!!!end_time) {
+                            return "";
+                        }
+                        if (end_time > Date.now()) {
+                            setTimeout(make_result, Math.min(Date.now() - end_time, 100));
+                            return "";
+                        }
+                    }
+                    console.log("make_result !!!");
+                    // to make result
+                    content = content + "\n" + await page.content();
+                    const cookies = await page.cookies(_fetch.url);
+                    try {
+                        result = {
+                            orig_url: _fetch.url,
+                            status_code: response.status() || 599,
+                            error: null,
+                            content: content,
+                            headers: response.headers(),
+                            url: page.url(),
+                            cookies: cookies,
+                            time: (Date.now() - start_time) / 1000,
+                            js_script_result: script_result,
+                            save: _fetch.save
+                        };
+                        console.log("["+result.status_code+"] "+result.orig_url+" "+result.time);
+                        finished = true;
+                    } catch(e) {
+                        result = {
+                            orig_url: _fetch.url,
+                            status_code: 599,
+                            error: e.toString(),
+                            content: content || "",
+                            headers: {},
+                            url: _fetch.url,
+                            cookies: {},
+                            time: (Date.now() - start_time) / 1000,
+                            js_script_result: null,
+                            save: _fetch.save
+                        }
+                    }
+                    resolve(result);
+                    await page.close();
+                },wait_before_end + 10);
+            })
+        };
 
-		// go to the page crawled
-		response = await page.goto(_fetch.url);
-		finish = await make_result();
-
-        // get <frame> and <iframe> tag content
-		// const iframes = await page.frames();
-		// for(let i in iframes){
-		// 	console.log(`this is ${i} iframe`);
-		// 	let iframe_content = await iframes[i].content();
-		// 	content = content + iframe_content + "\n";
-		// }
-
-		if(finish){
-			// run js_script
-			if(_fetch.js_script){
-				script_result = await page.evaluate(_fetch.js_script);
-				await make_result();
-			}
-
-			// to make result
-			content = content + "\n" + await page.content();
-			const cookies = await page.cookies(_fetch.url);
-			result = {
-				orig_url: _fetch.url,
-				status_code: response.status() || 599,
-				error: null,
-				content: content,
-				headers: response.headers(),
-				url: page.url(),
-				cookies: cookies,
-				time: (Date.now() - start_time) / 1000,
-				js_script_result: script_result,
-				save: _fetch.save
-			};
-			console.log("["+result.status_code+"] "+result.orig_url+" "+result.time);
-			finish = false;
-			// console.log("finish");
-		}else{
-			throw "Timeout to get page !"
-		}
-	}catch(e){
-		result = {
-			orig_url: _fetch.url,
-			status_code: 599,
-			error: e.toString(),
-			content: content || "",
-			headers: {},
-			url: _fetch.url,
-			cookies: {},
-			time: (Date.now() - start_time) / 1000,
-			js_script_result: null,
-			save: _fetch.save
-		}
-	}
-	// console.log(JSON.stringify(result, null, 2));
-	await page.close();
-    return JSON.stringify(result, null, 2);
+        response = await page.goto(_fetch.url);
+    });
 };
 
 // post method with request
