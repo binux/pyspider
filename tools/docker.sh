e91886315395329340be08b41ae6cb2c3e13351f
lizc <lizc@hill.today>
2019-01-31 12:37:53 +0800

    modify scripts.

--- tools/docker.sh
+++ tools/docker.sh
@@ -10,18 +10,18 @@ PROD_GROUP=ops
 PROD_NAME=pyspier
 PROD_TAG=${REGISTRY_TAG}/${PROD_GROUP}/${PROD_NAME}:${PROD_VERSION}
 
-echo "####################current product version ${PROD_TAG} ####################"
+echo "#################### Current Product Version: ${PROD_TAG} ####################"
 
 if [[ $1 = "run" ]]
 then
+    echo "#################### Docker Run ####################"
     docker run --restart=always -itd --name ${PROD_TAG} -p 5000:5000 -p 23333:23333 -p 24444:24444 -p 25555:25555 ${PROD_TAG}
 fi
 
 if [[ $1 = "stop" ]]
 then
-    echo "####################docker stop####################"
+    echo "#################### Docker Stop ####################"
     docker rm -f $(docker ps | grep ${PROD_TAG}| awk '{print $1}')
-    echo "#################### $2 stop done##################"
 fi
 
 if [[ $1 = "list" ]]
@@ -36,28 +36,28 @@ fi
 
 if [[ $1 = "go" ]]
 then
+    echo "#################### Build ${PROD_TAG} ####################"
     docker build -t ${PROD_TAG} .
-    echo "####################docker build done####################"
+    echo "#################### Pull ${PROD_TAG} ####################"
     docker push ${PROD_TAG}
-    echo "####################docker push done#####################"
 fi
 
 if [[ $1 = "build" ]]
 then
+    echo "#################### Build ${PROD_TAG} ####################"
     docker build -t ${PROD_TAG} .
-    echo "####################docker build done####################"
 fi
 
 if [[ $1 = "push" ]]
 then
-    echo "####################pull ${PROD_TAG} ####################"
+    echo "#################### Pull ${PROD_TAG} ####################"
     docker push ${PROD_TAG}
-    echo "####################pull done############################"
 fi
 
 if [[ $1 = "pull" ]]
 then
-    echo "####################pull ${PROD_TAG} ####################"
+    echo "#################### Pull ${PROD_TAG} ####################"
     docker pull ${PROD_TAG}
-    echo "####################pull done############################"
 fi
+
+echo "#################### DONE. ####################"
