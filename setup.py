0e3eaf42530c3e99df0aa31e376cc23b0d441088
Keith Tunstead <tunstek@tcd.ie>
2019-11-07 09:45:08 +0100

    removed python 3.8 from setup.py

--- setup.py
+++ setup.py
@@ -78,7 +78,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
 
         'License :: OSI Approved :: Apache Software License',
 
