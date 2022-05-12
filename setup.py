7b783b2660988a0f90197e87c4018f588acdf5a1
Jack <lusi@seadaka.com>
2022-05-12 15:59:44 +0800

    update

--- setup.py
+++ setup.py
@@ -67,6 +67,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
 
         'License :: OSI Approved :: Apache Software License',
 
